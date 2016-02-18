e98eaa676598e4a03a5e12446f250538cb494d4b
Chris Block <chris.block@gmail.com>
2016-02-18 09:16:42 -0600

    Update a large number of nuget packages.

--- source/Cadastre/Scripts/_references.js
+++ source/Cadastre/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="modernizr-2.7.2.js" />
+﻿/// <reference path="jquery-2.2.0.js" />
+/// <reference path="modernizr-2.8.3.js" />
 /// <reference path="bootstrap.js" />
-/// <reference path="jquery-2.1.0.js" />
 /// <reference path="knockout-3.1.0.js" />
